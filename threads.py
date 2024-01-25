e7fc036e518e52e5fddd3b6e4f691c03595bddca
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-01-25 20:41:50 +0000

    threads.py now places the servers on a list a returns it

--- threads.py
+++ threads.py
@@ -9,19 +9,32 @@ load_dotenv()
 
 
 def run_thread(num_bl_servers, num_db_servers):
+    server_list = []
     # Load Balancers
     thread1 = Thread(target=LoadBalancer, args=(os.getenv('DB_LOAD_BALANCER_IP'),))
     thread2 = Thread(target=LoadBalancer, args=(os.getenv('BL_LOAD_BALANCER_IP'),))
+
+    server_list.append([thread1, thread2])
 
     thread1.start()
     thread2.start()
 
     # Data Layer Servers
+    data_layer_list = []
     for i in range(num_db_servers):
         thread = Thread(target=DLServer)
+        data_layer_list.append(thread)
         thread.start()
 
+    server_list.append(data_layer_list)
+
     # Business Layer Servers
+    business_layer_list = []
     for i in range(num_bl_servers):
         thread = Thread(target=BLServer)
+        business_layer_list.append(thread)
         thread.start()
+
+    server_list.append(business_layer_list)
+
+    return server_list
